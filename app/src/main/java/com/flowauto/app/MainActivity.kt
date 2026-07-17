d85dc04b7cef143a46654b6659b5e5ce3bbdfc16
charfeddineAZ <charfedine68@gmail.com>
2026-07-17 13:04:04 +0000

    hifdgnb

---
+++ app/src/main/java/com/flowauto/app/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.flowauto.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppContent()
+        }
+    }
+}
+
+@Composable
+fun AppContent() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+            Text(text = "Welcome to FlowAuto")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppContentPreview() {
+    AppContent()
+}
